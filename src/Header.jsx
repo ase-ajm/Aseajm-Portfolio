247c237cdecd1e21f08fb467fd5eecf5233d7180
ase-ajm <asemain1014@gmail.com>
2026-09-20 16:58:31 -0400

    more updates

--- src/Header.jsx
+++ src/Header.jsx
@@ -23,7 +23,7 @@ function Header() {
           <a href="https://github.com/ase-ajm" target="_blank" rel="noopener noreferrer">GitHub</a>
           <a href="https://linkedin.com/in/aseajm" target="_blank" rel="noopener noreferrer">LinkedIn</a>
           <a href="/Resume_Asef_Ajmain_Master.pdf" target="_blank" rel="noopener noreferrer">Resume</a>
-          <a href="mailto:aseajm04@gmail.com">Email</a>
+          <a href="mailto:asemain1014@gmail.com">Email</a>
         </div>
       </div>
     </header>
